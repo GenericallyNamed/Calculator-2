96523f52122e42065a9aa0532b75cbf6b3a32d47
Alex <statefan0826@Gmail.com>
2020-07-04 23:29:50 -0400

    calculator time

---
+++ window.js
@@ -0,0 +1,21 @@
+const {remote} = require('electron');
+
+document.getElementById('closeBtn').addEventListener('click', closeWindow);
+document.getElementById('minimizeBtn').addEventListener('click', minimizeWindow);
+document.getElementById('maximizeBtn').addEventListener('click', maximizeWindow);
+console.log("identified elements");
+
+function closeWindow () {
+    var window = remote.BrowserWindow.getFocusedWindow();
+    window.close();
+}
+
+function minimizeWindow () {  
+    var window = remote.BrowserWindow.getFocusedWindow();
+    window.minimize();
+}
+
+function maximizeWindow () {
+    var window = remote.BrowserWindow.getFocusedWindow();
+    window.isMaximized() ? window.unmaximize() : window.maximize();
+}
